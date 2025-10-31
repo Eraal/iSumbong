e06d4e97f2cb1689bd0ac310095e02de5dd819f2
Gerald <geraldnazpandaan1@gmail.com>
2025-10-31 19:40:07 +0800

    Icon and Buttons

--- user/about/index.php
+++ user/about/index.php
@@ -19,6 +19,8 @@ if(logged_in()){
 
     <!-- Custom fonts for this template-->
     <link href="../../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <!-- Fallback CDN for Font Awesome in case local fonts fail to load -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     
     <!-- Custom styles for this template-->
